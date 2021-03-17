5b3e5f7f2a7b51e268fa4accebbaebbd021abd5e
Xyven <35360746+Xyven1@users.noreply.github.com>
2021-03-17 00:48:48 -0700

    Update 30.js

--- 30.js
+++ 30.js
@@ -1,5 +1,5 @@
 isFifth = n => n == n.toString().split('').map(n=>n**5).reduce((a,b)=> a+b)
 sum = 0
-for(a = 2; a<=1000000; a++)
-	if(isFifth(a)) sum +=a
+for(a = 2; a<=6*9**5; a++)
+	sum += isFifth(a)*a //non branching code. Woo hoo!
 console.log(sum)

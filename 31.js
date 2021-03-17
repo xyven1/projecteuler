8fd63b5066b7b36e298405def545f63ea110e592
Xyven <35360746+Xyven1@users.noreply.github.com>
2021-03-17 13:58:57 -0700

    Update 31.js

--- 31.js
+++ 31.js
@@ -2,7 +2,7 @@ const permute = (c, r, i = c.length-1) => {
 	if(r*i == 0) return 1
 	let total = 0
 	for(let n = 0; n<=r/c[i]; n++)
-    total += permute(c, r - n*c[i], i-1)
-  return total
+   		total += permute(c, r - n*c[i], i-1)
+	return total
 }
 console.log(permute([1, 2, 5, 10, 20, 50, 100, 200], 200))

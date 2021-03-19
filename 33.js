28823b6fc029f08782135cf480091adb6dcc65f3
XyvenQ <maritimus987@gmail.com>
2021-03-19 12:51:42 -0700

    less curly brackets

--- 33.js
+++ 33.js
@@ -1,10 +1,8 @@
 var n=1, d=1
-for(var i = 10; i<100; i++){
-  for(var j = 10; j<i; j++){
+for(var i = 10; i<100; i++)
+  for(var j = 10; j<i; j++)
     i.toString().split('').forEach(x=>{
       if(j.toString().indexOf(x)!=-1 && parseInt(j.toString().replace(x, ''))/parseInt(i.toString().replace(x, '')) == j /i && j%10!=0)
         [n,d] = [n*j, d*i]
     })
-  }
-}
 console.log(d/n)

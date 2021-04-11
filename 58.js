0cebb215fe3bc0cf551d600dc799803d0b9c4107
XyvenQ <maritimus987@gmail.com>
2021-04-10 17:07:29 -0700

    cleaned up 58

--- 58.js
+++ 58.js
@@ -1,4 +1,3 @@
-var t0 = process.hrtime()
 const eratosthenes = n => {
   var array = Array(n).fill(1), output = [2];
   for (var i = 3; i*i <= n; i += 2)
@@ -10,7 +9,7 @@ const eratosthenes = n => {
       output.push(i)
   return output
 }, primes = eratosthenes(1e5), isPrime = (num, primes) => { for(let i = 0; primes[i]**2 <= num; i++) if(num % primes[i] === 0) return 0; return num > 1}
-for(i = 1, prev = 1,prime = 0;; i++){
+for(var i = 1, prev = 1, prime = 0;; i++){
   prime += 1*isPrime(prev, primes)
 	prev = prev + 2*(Math.floor((i-1)/4)+1)
   if(i>1 && (prime/i)<.1){

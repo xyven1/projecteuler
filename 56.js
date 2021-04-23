e96f4562c8ff352f221ad227489af9980d8c6e4c
XyvenQ <maritimus987@gmail.com>
2021-04-22 22:21:38 -0700

    much cleaner 56

--- 56.js
+++ 56.js
@@ -1,9 +1,6 @@
 var max = 0;
-for (let i = 90n; i < 100n; i++) {
-  for (let j = 90n; j < 100n; j++) {
-    var sumOfDigits = (i**j).toString().split('').reduce((a,b)=> a+parseInt(b), 0)
-    max = (sumOfDigits > max)*sumOfDigits||max
-  }
-}
+for (let i = 90n; i < 100n; i++)
+  for (let j = 90n; j < 100n; j++)
+    max = Math.max(max, (i**j).toString().split('').reduce((a,b)=> a+parseInt(b), 0))
 console.log(max)
 //4.4ms

7fec2b23d4824ebf48fb515803c3e9892b48151e
Xyven <35360746+Xyven1@users.noreply.github.com>
2021-04-22 22:25:04 -0700

    corrected time on 56

--- 56.js
+++ 56.js
@@ -3,4 +3,4 @@ for (let i = 90n; i < 100n; i++)
   for (let j = 90n; j < 100n; j++)
     max = Math.max(max, (i**j).toString().split('').reduce((a,b)=> a+parseInt(b), 0))
 console.log(max)
-//4.4ms
+//2.4ms

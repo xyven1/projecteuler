9ea6f6821c1d85f186df3f9e5a6516f8d11e3945
XyvenQ <maritimus987@gmail.com>
2021-06-07 01:55:02 -0700

    statistical approach to 79

---
+++ 079.js
@@ -0,0 +1,57 @@
+const mode = (arr)=>arr.sort((a,b) => arr.filter(v => v===a).length - arr.filter(v => v===b).length).pop()
+var keyLog = `319
+680
+180
+690
+129
+620
+762
+689
+762
+318
+368
+710
+720
+710
+629
+168
+160
+689
+716
+731
+736
+729
+316
+729
+729
+710
+769
+290
+719
+680
+318
+389
+162
+289
+162
+718
+729
+319
+790
+680
+890
+362
+319
+760
+316
+729
+380
+319
+728
+716`.split('\n'), res = ''
+while(keyLog.length>0){
+  res+=mode(keyLog.map(v=>v[0]))
+  keyLog = keyLog.map(v=>v[0]==res[res.length-1] ? v.slice(1) : v).filter(v=>v!='')
+}
+console.log(res)
+//3.2ms

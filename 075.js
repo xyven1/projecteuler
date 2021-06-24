b5f2dcce2f0770a8ed05430b69eea8a14f364016
XyvenQ <maritimus987@gmail.com>
2021-06-23 21:04:11 -0700

    fixed gcd function

--- 075.js
+++ 075.js
@@ -1,4 +1,4 @@
-const gcd = (a, b) => a==b? a : a > b? gcd(a-b, b) : gcd(a, b-a),
+const gcd = (a, b) => b? gcd(b, a % b) : a,
 limit = 1.5e6
 var unique = Array(limit).fill(0)
 for(let m=2; m<Math.sqrt(limit/2); m++)
@@ -7,4 +7,4 @@ for(let m=2; m<Math.sqrt(limit/2); m++)
       for(let k = 1, p = 2*m*(m+n); p*k<limit; k++)
         unique[k*p]++
 console.log(unique.reduce((a,v)=>a+(v==1),0))
-//90ms
+//55ms
